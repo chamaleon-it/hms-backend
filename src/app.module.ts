3d7e9c0ec8171ccafb57211990d20e4049341ad8
rashidpkn <muhammed916rashid@gmail.com>
2025-10-08 16:55:58 +0530

    appoiment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import configuration from './config/configuration';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { AppointmentsModule } from './appointments/appointments.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { AuthModule } from './auth/auth.module';
     MongooseModule.forRoot(configuration().databaseUrl),
     UsersModule,
     AuthModule,
+    AppointmentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
