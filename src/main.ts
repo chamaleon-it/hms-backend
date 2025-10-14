5f6bf7c25324216d794518f6f4bdbea5476af803
rashidpkn <muhammed916rashid@gmail.com>
2025-10-14 16:18:12 +0530

    cors added

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000', // Next.js local
       'http://localhost:3001', // Optional local admin or test client
-      'https://your-production-domain.com', // Production frontend
+      'https://synapsehms.com', // Production frontend
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
