9c7808b0ebcc07085fbc2d67e89eaf3104af50d2
rashidpkn <muhammed916rashid@gmail.com>
2026-01-12 11:25:24 +0530

    port 2,3,4 were added

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,9 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000',
       'http://localhost:3001',
+      'http://localhost:3002',
+      'http://localhost:3003',
+      'http://localhost:3004',
       'https://synapsehms.com',
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
