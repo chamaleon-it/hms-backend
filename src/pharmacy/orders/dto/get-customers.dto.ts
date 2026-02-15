81e3568bbe15745c520917ea797165e26e7e7cc8
rashidpkn <muhammed916rashid@gmail.com>
2026-02-15 11:32:36 +0530

    feat: Add advanced filtering options for customers by search query, gender, doctor, last visit, and age.

--- src/pharmacy/orders/dto/get-customers.dto.ts
+++ src/pharmacy/orders/dto/get-customers.dto.ts
@@ -18,4 +18,41 @@ export class GetCustomersDto {
     @Transform(({ value }) => (value === '' ? undefined : value))
     @IsEnum(['true', 'false'])
     alreadyPurchase?: 'true' | 'false' = 'true';
+
+    @IsOptional()
+    q?: string; // phone number / patiner name / address / patient id
+
+    @IsOptional()
+    gender?: string; // Female, Male, Other
+
+    @IsOptional()
+    doctor?: string // mongose id
+
+    @IsOptional()
+    lastVisit?: string //  7 / 30 / Custom
+
+    @IsOptional()
+    from?: string // date if custom
+
+    @IsOptional()
+    to?: string // date if custom
+
+    @IsOptional()
+    age: string // `${filter.age[0]}-${filter.age[1]}`
+
+
+
 }
+
+
+//sample
+
+// alreadyPurchase false
+// page 1
+// limit 10
+// gender Other
+// doctor 6901f794e6543fa586e3cb0e
+// from 2026-02-17T18:30:00.000Z
+// to 2026-02-16T18:30:00.000Z
+// age 10-20
+// lastVisit Custom
