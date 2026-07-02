e2595a9c683a7c4e1e827334075597bdc767fa59
rashidpkn <muhammed916rashid@gmail.com>
2026-07-02 16:57:37 +0530

    refactor: disable status checks for pending and inactive users during authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -28,15 +28,16 @@ export class AuthService {
         'No user record found. Kindly register to proceed.',
       );
     }
-    if ((user.status as UserStatus) === UserStatus.PENDING) {
-      throw new BadRequestException(
-        'Your profile is under review. The verification process usually takes up to 24 hours.',
-      );
-    } else if ((user.status as UserStatus) === UserStatus.INACTIVE) {
-      throw new BadRequestException(
-        'This profile is inactive. Please contact the administrator for further assistance.',
-      );
-    } else if ((user.status as UserStatus) === UserStatus.BLOCKED) {
+    // if ((user.status as UserStatus) === UserStatus.PENDING) {
+    //   throw new BadRequestException(
+    //     'Your profile is under review. The verification process usually takes up to 24 hours.',
+    //   );
+    // } else if ((user.status as UserStatus) === UserStatus.INACTIVE) {
+    //   throw new BadRequestException(
+    //     'This profile is inactive. Please contact the administrator for further assistance.',
+    //   );
+    // } else 
+      if ((user.status as UserStatus) === UserStatus.BLOCKED) {
       throw new BadRequestException(
         'This profile is blocked. Please contact the administrator for further assistance.',
       );
