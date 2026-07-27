ee1451dcb2f07d4b8afce7955178885c21b75ec2
Muhammed Rashid N <muhammed916rashid@gmail.com>
2026-07-27 15:06:58 +0530

    fix: disable validation whitelisting in main.ts to allow additional request properties

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
+      // whitelist: true,
+      // forbidNonWhitelisted: true,
     }),
   );
 
