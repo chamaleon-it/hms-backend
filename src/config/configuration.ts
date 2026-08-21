b8cc7032daa84ee9642b873d72b6be1ad0296ad7
Muhammed Rashid N <muhammed916rashid@gmail.com>
2026-08-21 14:22:32 +0530

    refactor: cast JWT secret environment variables to string type in configuration

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,11 +1,11 @@
 export default () => ({
   databaseUrl: process.env.DATABASE_URL as string,
   secret: {
-    accessToken: process.env.JWT_ACCESS_SECRET,
+    accessToken: process.env.JWT_ACCESS_SECRET as string,
     refreshToken:
-      process.env.JWT_REFRESH_SECRET,
+      process.env.JWT_REFRESH_SECRET as string,
     forgotPassword:
-      process.env.JWT_FORGOT_SECRET,
+      process.env.JWT_FORGOT_SECRET as string,
   },
   in_house_lab_id: process.env.IN_HOUSE_LAB_ID,
   in_house_pharmacy_id: process.env.IN_HOUSE_PHARMACY_ID as string,
